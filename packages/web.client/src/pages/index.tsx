5df75841dce81fa8fddb9b329379c0414987fb62
Victor Kim <kim.victor.aleksandrovich@gmail.com>
2021-07-01 18:05:02 +0600

    feat(web.client): connect emotion, tailwindcss and twin.macro (#15)

--- packages/web.client/src/pages/index.tsx
+++ packages/web.client/src/pages/index.tsx
@@ -1,7 +1,8 @@
 import type { NextPage } from 'next';
+import tw from 'twin.macro';
 
 const message = 'hello';
 
-const HomePage: NextPage = () => <div>{message}</div>;
+const HomePage: NextPage = () => <div css={tw`text-yellow-100 bg-black`}>{message}</div>;
 
 export default HomePage;
