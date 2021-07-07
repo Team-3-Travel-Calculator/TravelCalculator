0e86dbfaf1f449b82e6e46490136a934b8b4b464
bota21 <aimkulow@bk.ru>
2021-07-07 09:57:35 +0600

    feat(web.service): add schema for user in mongo database(#20)

--- packages/web.service/src/main.ts
+++ packages/web.service/src/main.ts
@@ -3,12 +3,15 @@ import express from 'express';
 import { logger } from 'logger';
 import { resolve } from 'path';
 
+import mongoConnection from './services/connectMongo';
 import { isProduction } from './services/isProduction';
 
 export const main = (): void => {
   if (!isProduction()) config({ path: resolve(__dirname, '../.env') });
 
   const mainLogger = logger.getLogger('main');
+
+  mongoConnection().catch((error: string) => mainLogger.debug(error));
 
   const app = express();
 
