6afbde29a4319798d69cdd8e84782efa998f1c5b
Ибрагимова Татьяна <i.tatyana.a@gmail.com>
2021-07-26 21:00:34 +0600

    refactor(web.service): add changes to Hotel schemas

--- packages/web.service/src/modules/hotel/schema.ts
+++ packages/web.service/src/modules/hotel/schema.ts
@@ -1,20 +1,33 @@
 import { model, Schema } from 'mongoose';
 
-import { ServiceTypes } from '../comfortLevel';
+import type { Client } from '../client';
+import { ComfortLevels } from '../comfortLevel';
+import { HotelTypes } from '../hotelPrice';
+import { PersonsNumbers } from '../personsNumber';
 import { SeasonTypes } from '../season';
 
 export type Hotel = {
-  readonly hotelRoomId: number;
-  readonly serviceTypes: ServiceTypes;
-  readonly seasonTypes: SeasonTypes;
-  readonly price: number;
+  readonly client: Client;
+  readonly stayDate: Date;
+  readonly personsNumber: PersonsNumbers;
+  readonly hotelType: HotelTypes;
+  readonly seasonType: SeasonTypes;
+  readonly comfortLevel: ComfortLevels;
+  readonly tripleRoomNumber: number;
+  readonly doubleRoomNumber: number;
+  readonly singleRoomNumber: number;
 };
 
-export const schema = new Schema<Hotel>({
-  hotelRoomId: { type: Number, required: true },
-  serviceTypes: { type: Number, enum: Object.values(ServiceTypes), required: true },
-  seasonTypes: { type: Number, enum: Object.values(SeasonTypes), required: true },
-  price: { type: Number, required: true },
+const schema = new Schema<Hotel>({
+  client: { type: Schema.Types.ObjectId, ref: 'Client', required: true },
+  stayDate: { type: Date, required: true },
+  personsNumber: { type: Number, enum: Object.values(PersonsNumbers), required: true },
+  hotelType: { type: Number, enum: Object.values(HotelTypes), required: true },
+  seasonType: { type: Number, enum: Object.values(SeasonTypes), required: true },
+  comfortLevel: { type: Number, enum: Object.values(ComfortLevels), required: true },
+  tripleRoomNumber: { type: Number, required: true },
+  doubleRoomNumber: { type: Number, required: true },
+  singleRoomNumber: { type: Number, required: true },
 });
 
 export const HotelModel = model<Hotel>('Hotel', schema);
