03937f4dcbf5d2c50bc437a35a228672505ec3df
Ибрагимова Татьяна <i.tatyana.a@gmail.com>
2021-08-02 18:40:24 +0600

    refactor(web.service): add changes to Guide schemas, change files to js

---
+++ packages/web.service/src/modules/guideTypes/guideTypes.js
@@ -0,0 +1,12 @@
+export const GuideGroupSizes = {
+  Small: { code: 1, sizeLimit: 5 },
+  Medium: { code: 2, sizeLimit: 15 },
+  Large: { code: 3, sizeLimit: 50 },
+};
+
+export const GuideTypes = {
+  Transfer: 1,
+  Tour: 2,
+  Instructor: 3,
+  SkiInstructor: 4,
+};
