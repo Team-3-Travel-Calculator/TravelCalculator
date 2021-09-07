fab5f5da2224393ca16e0dd9fe476ce5bdb2b7dc
Ибрагим Асаев <ibragimilita@gmail.com>
2021-09-08 02:43:29 +0600

    feat(web.client): add _app.jsx file with global state

---
+++ packages/web.client/src/pages/_app.jsx
@@ -0,0 +1,14 @@
+import '../../styles.css';
+import 'tailwindcss/tailwind.css'
+
+import {Provider} from 'next-auth/client';
+
+// JSX Components can ONLY be used when identifier is in UpperCase
+// eslint-disable-next-line @typescript-eslint/naming-convention
+const App = ({Component, pageProps}) => (
+  <Provider session={pageProps.session}>
+    <Component {...pageProps} />
+  </Provider>
+);
+
+export default App;
